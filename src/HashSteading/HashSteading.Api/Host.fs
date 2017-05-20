5585099595904a2e38e51043897ba9dd22581d91
armadilloNik <nik@tumpedover.com>
2017-05-19 23:09:19 -0500

    standing up a simple suave listener

---
+++ src/HashSteading/HashSteading.Api/Host.fs
@@ -0,0 +1,13 @@
+﻿module Host 
+
+    open System
+    open System.Threading
+    open Suave
+    
+    let Start =
+        let cts = new CancellationTokenSource()
+        let conf = { defaultConfig with cancellationToken = cts.Token }
+        let listening, server = startWebServerAsync conf (Successful.OK "Hello World")
+    
+        Async.Start(server, cts.Token)
+        cts
