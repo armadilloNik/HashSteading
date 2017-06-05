1615575ae4528eaa924205dc2821de9ed04ede6b
armadilloNik <nik@tumpedover.com>
2017-06-05 17:20:58 -0500

    establishing cmd loop

--- src/HashSteading/Hst/Program.fs
+++ src/HashSteading/Hst/Program.fs
@@ -1,16 +1,39 @@
 ﻿open System
 
+
+let prompt() =
+    Console.Write ":>> " |> ignore
+    let input = Console.ReadLine() 
+    input
+
+let rec promptMonitor() =
+    let input = prompt().ToLower()
+    match input with
+        | "exit" -> ()
+        | "repo" -> Console.WriteLine "repo!"
+                    promptMonitor()
+        | _ -> promptMonitor()
+          
+    //if input = "exit" then ()
+    //else promptMonitor()
+
 [<EntryPoint>]
 let main argv = 
-  
-    let path = @"c:\temp\HashSteading\"
-
-    let token = path 
-                |> Repo.GetFileList 
-                |> Host.Start
-
-    Console.ReadKey true |> ignore
     
-    token.Cancel()
+    promptMonitor()
+    
+
+    Console.WriteLine "done!" |> ignore
+    Console.ReadKey true |> ignore
+
+    //let path = @"c:\temp\HashSteading\"
+
+    //let token = path 
+    //            |> Repo.GetFileList 
+    //            |> Host.Start
+
+    //Console.ReadKey true |> ignore
+    
+    //token.Cancel()
 
     0 
